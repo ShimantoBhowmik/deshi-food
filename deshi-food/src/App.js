5dd3d56ec75ae96c9a2c4465525f8e55d5b36f67
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-05-30 10:01:42 -0400

    Initializing the foods for the restaurant

--- deshi-food/src/App.js
+++ deshi-food/src/App.js
@@ -1,8 +1,37 @@
 
 const App = () => {
   return (
-    <div >
-      Hello 
+    <div className = "foods">
+      <div className = "food">
+      <div className = "food-body">
+        <h3> Rice </h3>
+        <a> Add this delight to your Cart Now!</a>
+      </div>
+      </div>
+      <div className = "food">
+      <div className = "food-body">
+        <h3> Curry </h3>
+        <a> Add this delight to your Cart Now!</a>
+      </div>
+      </div>
+      <div className = "food">
+      <div className = "food-body">
+        <h3> Biriyani </h3>
+        <a> Add this delight to your Cart Now!</a>
+      </div>
+      </div>
+      <div className = "food">
+      <div className = "food-body">
+        <h3> Dessert </h3>
+        <a> Add this delight to your Cart Now!</a>
+      </div>
+      </div>
+      <div className = "food">
+      <div className = "food-body">
+        <h3> Snacks </h3>
+        <a> Add this delight to your Cart Now!</a>
+      </div>
+      </div>
     </div>
   );
 }
