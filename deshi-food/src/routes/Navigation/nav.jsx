059662c7983b75dda4eec224867a6b142941f336
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-01 09:33:02 -0400

    Implemented Sign Out Feature

--- deshi-food/src/routes/Navigation/nav.jsx
+++ deshi-food/src/routes/Navigation/nav.jsx
@@ -3,26 +3,39 @@ import { Outlet, Link } from 'react-router-dom';
 
 import {ReactComponent as Logo} from '../../assets/logo.svg'
 import { UserContext } from '../../contexts/user';
+import { signOutUser } from '../../utils/firebase/firebase';
 
 import './nav.scss';
 
 const Nav = () => {
-  const { currUser } = useContext(UserContext);
+  const { currUser, setCurrUser } = useContext(UserContext);
   
-  
+  const signOutHandler = async() =>{
+    await signOutUser();
+    setCurrUser(null);
+  }
+
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+            <Logo className="logo" />
         </Link>
-        <div className='links'>
-          <Link className='link' to='/shop'>
+        <div className="links">
+          <Link className="link" to='/shop'>
             SHOP
           </Link>
-          <Link className='link' to='/sign-in'>
-            SIGN IN
-          </Link>
+          {
+            currUser ? (
+              <span className="link" onClick={signOutHandler}>
+                SIGN OUT
+              </span>
+            ) :(
+              <Link className="link" to='/sign-in'>
+                SIGN IN
+              </Link>
+            )
+          }
         </div>
       </div>
       <Outlet />
