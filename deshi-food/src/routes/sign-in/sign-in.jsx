8223b2468ff2eac0ed6d88a2055c999728c82ba9
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-01 07:43:41 -0400

    Added sign in , sign up and google sign in functionality and styling

--- deshi-food/src/routes/sign-in/sign-in.jsx
+++ deshi-food/src/routes/sign-in/sign-in.jsx
@@ -1,21 +1,17 @@
 
 import { signInWithGooglePopup,createUserDoc } from "../../utils/firebase/firebase";
 import SignUp from "../../components/sign-up/sign-up";
-
+import SignInForm from "../../components/sign-in/sign-in";
 
 const SignIn = () => {
-
-    const logInGoogleUser = async () =>{
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDoc(user);
-    }
-    
 
     return(
         <div>
             <h1>Sign in page</h1>
-            <button onClick={logInGoogleUser}>Sign in with Google Popup</button>
+            {/* <button onClick={logInGoogleUser}>Sign in with Google Popup</button> */}
+            <SignInForm/>
             <SignUp/>
+            
         </div>
     )
 }
