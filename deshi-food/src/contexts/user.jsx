5059a96c992a71275c5aefe6ac6d29943a61ebe6
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-01 11:45:52 -0400

    Integrating Firebase onAuthStateChanged

--- deshi-food/src/contexts/user.jsx
+++ deshi-food/src/contexts/user.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { Listener } from "../utils/firebase/firebase";
 
 export const UserContext = createContext({
     currUser: null,
@@ -9,5 +10,13 @@ export const UserProvider = ({ children }) => {
     const[currUser, setCurrUser] = useState(null);
     const val= { currUser, setCurrUser};
 
+    useEffect(() => {
+        const stopListening = Listener((user) =>{
+            console.log(user);
+        })
+
+        return stopListening;
+    }, []);
+    
     return <UserContext.Provider value={val}>{children}</UserContext.Provider>
 }
