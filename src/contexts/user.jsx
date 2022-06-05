ff2f84c8685db34dd0e622e9cf43e8c7105747e1
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-05 08:38:02 -0400

    Implemented React Reducer in user context

--- src/contexts/user.jsx
+++ src/contexts/user.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { Listener,createUserDoc } from "../utils/firebase/firebase";
 
 export const UserContext = createContext({
@@ -6,8 +6,42 @@ export const UserContext = createContext({
     setCurrUser: () => null,
 });
 
+
+export const ACTION_TYPES = {
+    SET_CURRENT_USER : 'SET_CURRENT_USER'
+}
+
+
+const userReducer = (state, action) => {
+    
+    const {type, payload } = action;
+
+    switch(type){
+        case ACTION_TYPES.SET_CURRENT_USER:
+            return{
+                ...state,
+                currUser: payload
+        }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+
+    }
+}
+
+const INITIAL_STATE = {
+    currUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const[currUser, setCurrUser] = useState(null);
+    
+    
+    const[{currUser}, dispatch] = useReducer(userReducer, INITIAL_STATE );
+    
+
+    const setCurrUser = (user) => {
+        dispatch({type:ACTION_TYPES.SET_CURRENT_USER, payload: user});
+    }
+
     const val= { currUser, setCurrUser};
 
     useEffect(() => {
