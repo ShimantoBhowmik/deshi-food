b97301ffa7fd80d9eab1c2a98c05493559dbee8f
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-07 17:47:32 +0600

    Implemented React thunk for checking isLoading

--- src/store/store.js
+++ src/store/store.js
@@ -5,15 +5,15 @@ import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 import {persistStore, persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+import thunk from 'redux-thunk';
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
   Boolean
 );
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  whitelist: ['cart'],
 
 }
 
