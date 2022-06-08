75fb019a5fdffb04707e21815aeebc1bfbed49a9
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-08 11:00:55 +0600

    Integrated netlify functions and first stripe payment successful on developers dashboard

--- src/components/payments/payments.jsx
+++ src/components/payments/payments.jsx
@@ -13,15 +13,42 @@ const Payment = () => {
         if(!stripe ||  !elements){
             return;
         }
+        
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000})
+        }).then( res => res.json());
 
+
+        const clientSecret = response.paymentIntent.client_secret;
+        
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Bla Bla'
+                }
+            }
+        });
+
+        if(paymentResult.error){
+            alert(paymentResult.error);
+        }else{
+            if(paymentResult.paymentIntent.status ==='succeeded'){
+                alert('Payment Successful');
+            }
+        }
         
 
-    }
+    };
 
 
     return(
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={handlePayment}>
                 <h3> Credit Card Payment </h3>
                 <CardElement></CardElement>
                     <Button> Pay </Button>
